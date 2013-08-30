df9a0377cd4c5cb37cc0d1829fc9c33cc13b739e
Xiaoqiang Wang <xiaoqiangwang@users.noreply.github.com>
2013-08-30 11:36:41 +0200

    reorder statement

---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Setup file for Ca-Python using distutils package.
+Python2.4 or later should be used.
+"""
+
+import os
+import platform
+from distutils.core import setup, Extension
+
+EPICSBASE=os.path.join(os.getcwd(), 'epicsbase')
+
+HOSTARCH=os.popen(os.path.join(EPICSBASE,"startup","EpicsHostArch.pl")).read()
+
+try:
+    UNAME=platform.uname()[0]
+except:
+    UNAME="Unknowon"
+
+if UNAME.lower() == "alpha":
+    libraries=["ca","As","Com"]
+elif UNAME.lower() == "win32":
+    libraries=["ca","Com","ws2_32","msvcrt","user32", "advapi32"]
+elif UNAME.lower() == "darwin":
+    libraries=["ca","Com","readline"]
+else:
+    libraries=["ca","Com","readline","rt"]
+
+
+rev="2.0"
+CA_SOURCE="src/_ca314.cpp" # for threaded version.
+setup(name="CaChannel",
+      version=rev,
+      author="Xiaoqiang Wang",
+      author_email = "xiaoqiang.wang AT psi DOT ch",
+      description="CaChannel Interface to EPICS",
+      long_description=open('README').read(),
+      url="http://cachannel.googlecode.com",
+      download_url="http://www-acc.kek.jp/EPICS_Gr/products.html",
+      classifiers=['Programming Language:: C, Python',
+                  'Topic :: EPICS CA',
+                  'Topic :: Controls'],
+      package_dir={"": "src"},
+      py_modules=["ca", "caError", "cadefs","_ca_kek", "_ca_fnal", "CaChannel"],
+      ext_modules=[Extension("_ca",[CA_SOURCE],
+                             include_dirs=[os.path.join(EPICSBASE,"include"),
+                                           os.path.join(EPICSBASE,"include", "os",UNAME),
+                                           os.path.join(EPICSBASE,"include", "os"),],
+                             define_macros=[("PYCA_VERSION",'"\\"%s\\""'%rev),
+                                            (UNAME, None)],
+                             undef_macros="WITH_TK",
+                             libraries=libraries,
+                             library_dirs=[os.path.join(EPICSBASE,"lib",HOSTARCH),],
+                             runtime_library_dirs=[os.path.join(EPICSBASE,"lib",HOSTARCH),],
+                            )
+                  ],
+    )
