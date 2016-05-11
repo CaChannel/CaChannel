0b94ac6390e569f7d192ee314d4fef4f3a10f5a2
XiaoqiangWang <xiaoqiangwang@gmail.com>
2016-05-11 20:10:04 +0200

    1 tab to 8 spaces

--- tests/ca_wf.py
+++ tests/ca_wf.py
@@ -3,10 +3,10 @@
 # filename: ca_io.py
 #
 # Test CaChannel io using ca.pend_io()
-#	search
-#	array_put
-#	array_get
-#	pend_io
+#       search
+#       array_put
+#       array_get
+#       pend_io
 #
 # Each group of actions (search, put, get) is executed by the call
 # to pend_io().  When the actions have completed pend_io() returns
@@ -29,25 +29,25 @@ def getCallback(epicsArgs, userArgs):
 
 def main():
     try:
-	cawave = CaChannel()
+        cawave = CaChannel()
 
-	cawave.searchw('cawave')
-	
-	t = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
+        cawave.searchw('cawave')
+        
+        t = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
         l = []
         for i in range(0,500):
             l.append(i)
-	#cawave.array_put(t)
-	#cawave.pend_io()
-	
+        #cawave.array_put(t)
+        #cawave.pend_io()
+        
         cawave.putw(l)
-	print cawave.getw()
-	
+        print cawave.getw()
+        
         cawave.add_masked_array_event(None, None, ca.DBE_VALUE, getCallback, 0)
         cawave.pend_event()
         #cawave.clear_event()
 
     except CaChannelException, status:
-	print ca.message(status)
+        print ca.message(status)
     
 main()
