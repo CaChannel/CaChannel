8812b2b2ee51c4c21df1d141e09809fc9e0b8898
XiaoqiangWang <xiaoqiangwang@gmail.com>
2016-06-24 12:53:44 +0200

    caffi as an optional backend and fallback

--- src/CaChannel/ca.py
+++ src/CaChannel/ca.py
@@ -1,1 +1,13 @@
-from ._ca import *
+import os
+import warnings
+if os.environ.get('CACHANNEL_BACKEND') == 'caffi':
+    from caffi.ca import *
+    from caffi.macros import *
+else:
+    try:
+        from ._ca import *
+    except:
+        warnings.warn("c extension is not available, trying caffi as fallback", RuntimeWarning)
+        from caffi.ca import *
+        from caffi.macros import *
+
