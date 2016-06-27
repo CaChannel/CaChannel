c39658879a80097a605d1492bacab9df56c48b4e
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2016-06-27 21:46:31 +0200

    bump version 3.0.0a3

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ else:
     print("Platform", UNAME, ARCH, " Not Supported")
     sys.exit(1)
 
-rev="3.0.0a2"
+rev="3.0.0a3"
 
 define_macros = [("PYCA_VERSION",'"\\"%s\\""'%rev), (UNAME, None)]
 include_dirs = [os.path.join(EPICSBASE,"include"),
