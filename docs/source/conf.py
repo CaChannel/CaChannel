c517e0877389af582475db5f91643a9eaf69aeaf
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-02-27 20:36:33 +0100

    extend copy right

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CaChannel'
-copyright = u'2013-2016, Xiaoqiang Wang'
+copyright = u'2013-2017, Xiaoqiang Wang'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
