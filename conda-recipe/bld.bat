9d35b831a61907c11a228a7b50170758c91012a0
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-02-27 21:09:53 +0100

    build egg/wheel in addition

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -1,4 +1,7 @@
-"%PYTHON%" setup.py install
+for /f "delims=" %%i in ('conda info --root') do set OUTPUT_PATH=%%i\conda-bld\win-%ARCH%
+
+"%PYTHON%" setup.py install bdist_wheel
+copy dist\*.whl %OUTPUT_PATH%
 if errorlevel 1 exit 1
 
 xcopy %RECIPE_DIR%\caRepeater.exe %PREFIX%\bin\
--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+OUTPUT_PATH=$(dirname $(conda build --output conda-recipe))
 
 case `uname` in
     Darwin )
-        CAREPEATER=$RECIPE_DIR/caRepeater.Darwin ;;
+        CAREPEATER=$RECIPE_DIR/caRepeater.Linux$ARCH
+        $PYTHON setup.py install bdist_wheel
+        cp dist/*.whl ${OUTPUT_PATH}
+        ;;
     Linux )
-        CAREPEATER=$RECIPE_DIR/caRepeater.Linux$ARCH ;;
+        CAREPEATER=$RECIPE_DIR/caRepeater.Darwin
+        $PYTHON setup.py install sdist bdist_egg
+        cp dist/*.tar.gz ${OUTPUT_PATH}
+        cp dist/*.egg ${OUTPUT_PATH}
+        ;;
     * )
         echo "Not Supported"
 esac
