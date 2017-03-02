be8e2b5afd005236837f3283d301f848fd7d1fbf
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2017-03-02 17:26:28 +0100

    refactor the test script

--- conda-recipe/run_test.py
+++ conda-recipe/run_test.py
@@ -4,78 +4,68 @@ import os
 import platform
 import socket
 import sys
+import time
 import re
 import runpy
 import subprocess
 import doctest
 import unittest
 
-print('Launch softIoc')
-try:
-    UNAME=platform.uname()[0]
-    ARCH=platform.architecture()[0]
-except:
-    UNAME="Unknown"
-    ARCH="Unknown"
+class TestCa(unittest.TestCase):
+    def setUp(self):
+        try:
+            UNAME=platform.uname()[0]
+            ARCH=platform.architecture()[0]
+        except:
+            UNAME="Unknown"
+            ARCH="Unknown"
 
-if UNAME.lower() == "windows":
-    if ARCH=="64bit":
-        HOSTARCH="windows-x64"
-    else:
-        HOSTARCH="win32-x86"
-    print("Test on windows has been disabled for the moment.")
-    sys.exit(0)
-elif UNAME.lower() == "darwin":
-    HOSTARCH = 'darwin-x86'
-elif UNAME.lower() == "linux":
-    if ARCH=="64bit":
-        HOSTARCH="linux-x86_64"
-    else:
-        HOSTARCH="linux-x86"
-else:
-    print("Platform", UNAME, ARCH, " Not Supported")
-    sys.exit(1)
+        if UNAME.lower() == "windows":
+            if ARCH=="64bit":
+                HOSTARCH="windows-x64"
+            else:
+                HOSTARCH="win32-x86"
+        elif UNAME.lower() == "darwin":
+            HOSTARCH = 'darwin-x86'
+        elif UNAME.lower() == "linux":
+            if ARCH=="64bit":
+                HOSTARCH="linux-x86_64"
+            else:
+                HOSTARCH="linux-x86"
+        else:
+            raise RuntimeError("Platform % is not supported"%UNAME)
 
-ip = socket.gethostbyname(socket.gethostname())
-print("IP Address is %s"%ip)
+        EPICS_BIN = os.path.join(os.environ['PREFIX'], 'epics', 'bin', HOSTARCH)
+        EPICS_DBD = os.path.join(os.environ['PREFIX'], 'epics', 'dbd')
 
-softIoc = subprocess.Popen(['softIoc', '-D', os.path.join(os.environ['PREFIX'], 'epics', 'dbd', 'softIoc.dbd') ,'-d', 'tests/test.db'],
-        shell = True,
-        stdin = subprocess.PIPE,
-        stdout = subprocess.PIPE,
-        env = {'PATH': os.path.join(os.environ['PREFIX'], 'epics', 'bin', HOSTARCH),
-            'EPICS_CA_AUTO_ADDR_LIST': 'NO',
-            'EPICS_CA_ADDR_LIST': ip,
-            }
+        environ = os.environ.copy()
+        environ['PATH'] += os.pathsep + EPICS_BIN
+
+        self.softIoc = subprocess.Popen(['softIoc', '-D', os.path.join(EPICS_DBD, 'softIoc.dbd'), '-d', 'tests/test.db'],
+                stdin = subprocess.PIPE,
+                stdout = subprocess.PIPE,
+                env = environ
         )
+        time.sleep(2)
 
-print('Run ca_test.py')
-try:
-    if UNAME.lower() != 'linux':
-        runpy.run_path('tests/ca_test.py', run_name='__main__')
-except SystemExit as e:
-    ecode = e.code
-else:
-    ecode = 0
+    def test_ca(self):
+        try:
+            if platform.system() != 'Linux':
+                runpy.run_path('tests/ca_test.py', run_name='__main__')
+        except SystemExit as e:
+            ecode = e.code
+        else:
+            ecode = 0
+        self.assertEqual(ecode, 0)
 
-if ecode != 0:
-    softIoc.terminate()
-    sys.exit(ecode)
+    def test_CaChannel(self):
+        test = doctest.DocTestSuite('CaChannel.CaChannel')
+        runner = unittest.TextTestRunner()
+        runner.run(test)
 
-print('Run CaChannel')
-try:
-    test = doctest.DocTestSuite('CaChannel.CaChannel')
-    runner = unittest.TextTestRunner()
-    runner.run(test)
-except SystemExit as e:
-    ecode = e.code
-else:
-    ecode = 0
+    def tearDown(self):
+        self.softIoc.stdin.write('exit\n')
+        time.sleep(2)
 
-if ecode != 0:
-    softIoc.terminate()
-    sys.exit(ecode)
-
-softIoc.terminate()
-sys.exit(0)
-
+if __name__ == '__main__':
+    unittest.main()
